8bb5a231610d4a8d51928fa00b42ea0bf9418e43
Jon Zachary <jontzachary@gmail.com>
2017-01-31 15:45:03 -0500

    add get all movie titles route and show movie details route

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var movies = require('./routes/movies');
 var index = require('./routes/index');
 var users = require('./routes/users');
 require('dotenv').config();
-
 var app = express();
 
 // view engine setup
@@ -31,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
+app.use('/movies',movies);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
