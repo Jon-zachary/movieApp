547f4f6199d921014b92ce63c7e284795cad3401
Jon Zachary <jontzachary@gmail.com>
2017-01-31 18:46:40 -0500

    add all routes and broken buttons

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ var bodyParser = require('body-parser');
 var movies = require('./routes/movies');
 var index = require('./routes/index');
 var users = require('./routes/users');
+var methodOverride = require('method-override');
 require('dotenv').config();
 var app = express();
+app.use(methodOverride('_method'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
