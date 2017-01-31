686ba5a7f3baa9fbd890ac6afcb2361ecb9cb42e
Jon Zachary <jontzachary@gmail.com>
2017-01-31 18:46:40 -0500

    add all routes and broken buttons

--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,17 @@ var express = require('express');
 var router = express.Router();
 var models = require('../db/models/index.js');
 
+router.delete('/:id', function(req,res,next){
+  models.User.destroy({
+    where: {id: req.params.id}
+  }).then(function(user) {
+    res.redirect('/users');
+  });
+});
 
+router.get('/new', function(req, res, next) {
+  res.render("movies/new");
+});
 //defines a route for the /movie page that passes the movies array to the ejs 
 //page where we use forEach to display the title.
 router.get('/', function(req, res, next) {
@@ -24,6 +34,26 @@ router.get('/:id', function(req, res, next) {
 router.get('/:id/edit', function(req, res, next) {
   models.Movie.findById(req.params.id).then(function(movie) {
     res.render('movies/edit', { movie: movie });
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+  models.Movie.update({
+    title: req.body.title,
+    synopsis: req.body.synopsis,
+  }, { where: { id: req.params.id } }).then(function() {
+    res.redirect('/movies/' + req.params.id);
+  });
+});
+
+
+
+router.post('/', function(req, res, next) {
+  models.Movie.create({
+    title: req.body.title,
+    synopsis: req.body.synopsis
+  }).then(function(movies) {
+    res.redirect('/movies');
   });
 });
 
