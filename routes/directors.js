dce3461ba5835b9c4314c3b21fb9508593fa661d
val rigodon <rvrigodon@gmail.com>
2017-01-31 19:15:14 -0500

    add,delete, and see directors

---
+++ routes/directors.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../db/models/index');
+
+/* GET users listing. */
+router.get('/', function(req,res,next){
+  models.Director.findAll({})
+    .then(function(directors){
+      res.render('directors/directors', {
+        name: directors.name,
+        directors: directors,
+        id: directors.id
+      });
+    });
+});
+
+router.post('/', function(req, res, next) {
+  models.Director.create({
+    name: req.body.name
+  }).then(function() {
+    res.redirect('directors/directors')
+  });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('directors/newdirectors');
+});
+
+
+router.get('/:id', function(req, res, next) {
+  models.Director.findById(req.params.id).then(function(directors) {
+    res.render('directors/showdirectors', {directors: directors });
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  models.Director.destroy({
+    where: { id: req.params.id }
+  }).then(function(directors) {
+    res.redirect('directors/directors');
+  });
+});
+
+
+
+
+module.exports = router;
