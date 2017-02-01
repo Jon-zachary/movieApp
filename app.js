1d2682f30cbe878167e18b710872a207b654a8bd
val rigodon <rvrigodon@gmail.com>
2017-02-01 17:03:08 -0500

    yes

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var directors= require('./routes/directors');
 
 var movies = require('./routes/movies');
 var index = require('./routes/index');
-var users = require('./routes/users');
 var methodOverride = require('method-override');
 
 require('dotenv').config();
@@ -39,7 +38,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.use('/users', users);
 app.use('/directors', directors);
 
 
