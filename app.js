73a8d1f3425962596da14c8b00ecf609c37a52bc
Treniese Ladson <ladsont10@gmail.com>
2017-01-31 19:11:21 -0500

    all but delete

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var movies = require('./routes/movies');
+var methodOverride = require('method-override');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 require('dotenv').config();
 
 var app = express();
+app.use(methodOverride('_method'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/movies',movies);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
