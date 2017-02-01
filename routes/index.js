75a3ecb9075d227b0711cab0259e1d68332f18e9
val rigodon <rvrigodon@gmail.com>
2017-01-31 19:15:14 -0500

    add,delete, and see directors

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../db/models/index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/direc', function(req, res, next) {
+  res.send('test');
+});
+
 
 module.exports = router;
