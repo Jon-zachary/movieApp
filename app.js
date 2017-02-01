f9a09be6b53d942077db4eb6513838c986c26dcb
val rigodon <rvrigodon@gmail.com>
2017-02-01 12:57:33 -0500

    deleting unnecessary files

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(require('node-sass-middleware')({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+
 app.use('/users', users);
 app.use('/directors', directors);
 
